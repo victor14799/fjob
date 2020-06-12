78939dde3d37d6a6e6d13ecad06b709c93edc28e
DoNguyen227 <donguyen.higashi@gmail.com>
2020-06-12 11:05:41 +0700

    fix query

--- src/main/java/com/example/fjob/lib/mapper/AccountRegisterMapper.java
+++ src/main/java/com/example/fjob/lib/mapper/AccountRegisterMapper.java
@@ -2,15 +2,45 @@ package com.example.fjob.lib.mapper;
 
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.example.fjob.lib.dataset.AccountRegisterDataset;
 
 @Mapper
 public interface AccountRegisterMapper {
 
-	@Insert("INSERT INTO public.account\r\n" + 
-			"(addr, email, first_name, \"password\", phone_no, user_name, last_name, img, ranked, is_student, is_verify, \"role\", balance, del_flg, ins_date)\r\n" + 
-			"VALUES('sss', 'sss', 'sss', 'sss', 'sss', 'sss', 'sss', 'sss', 'sss', '0', '0', 'sss', '0', '0',clock_timestamp());") 
-	int insertNewAccount(AccountRegisterDataset account);
+	@Insert("INSERT INTO public.account" + 
+			"(addr,"
+			+ " email,"
+			+ " first_name,"
+			+ " password,"
+			+ " phone_no,"
+			+ " user_name,"
+			+ " last_name,"
+			+ " img,"
+			+ " ranked,"
+			+ " is_student,"
+			+ " is_verify,"
+			+ " role,"
+			+ " balance,"
+			+ " del_flg,"
+			+ " ins_date)" + 
+			"VALUES("
+			+ " #{accountt.addr},"
+			+ " #{accountt.email},"
+			+ " #{accountt.firstName},"
+			+ " #{accountt.password},"
+			+ " #{accountt.phoneNo},"
+			+ " #{accountt.userName},"
+			+ " #{accountt.lastName},"
+			+ " #{accountt.img},"
+			+ " #{accountt.ranked},"
+			+ " #{accountt.isStudent},"
+			+ " #{accountt.isVerify},"
+			+ " #{accountt.role},"
+			+ " '0',"
+			+ " '0',"
+			+ "clock_timestamp())") 
+	int insertNewAccount(@Param("accountt") AccountRegisterDataset accountt);
 	
 }
