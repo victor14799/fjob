926208b0ed78b8439aae28611dd907469a9ad37d
NamNT <vuilachinh66@gmail.com>
2020-06-14 22:07:21 +0700

    [JOB_HISTORY] Commit all func
    (cherry picked from commit 85470feadabb6ebab75c65e32cb85d1b54ad2b7f)

---
+++ src/main/java/com/example/fjob/lib/component/jobhistory/JobHistoryComponentImp.java
@@ -0,0 +1,56 @@
+package com.example.fjob.lib.component.jobhistory;
+
+import com.example.fjob.lib.dataset.jobhistory.JobHistoryDataset;
+import com.example.fjob.lib.dataset.jobhistory.JobParamDataset;
+import com.example.fjob.lib.mapper.jobhistory.JobHistoryMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class JobHistoryComponentImp implements JobHistoryComponent {
+
+    /**
+     * JobHistoryMapper
+     */
+    private JobHistoryMapper mapper;
+
+    @Autowired
+    public JobHistoryComponentImp(JobHistoryMapper mapper) {
+        this.mapper = mapper;
+    }
+
+    /**
+     * getJobHistory
+     *
+     * @param bidUser
+     * @return List<JobHistoryDataset>
+     */
+    @Override
+    public List<JobHistoryDataset> getJobHistory(String bidUser) {
+        return mapper.getJobHistory(bidUser);
+    }
+
+    /**
+     * addJob
+     *
+     * @param paramDataset
+     * @return boolean
+     */
+    @Override
+    public boolean addJob(JobParamDataset paramDataset) {
+        return mapper.addJob(paramDataset) > 0;
+    }
+
+    /**
+     * updateFeedback
+     *
+     * @param paramDataset
+     * @return boolean
+     */
+    @Override
+    public boolean updateFeedback(JobParamDataset paramDataset) {
+        return mapper.updateFeedback(paramDataset) > 0;
+    }
+}
