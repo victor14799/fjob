e33946f8b3bd85c128964798cec3ea9cf44cdda7
NamNT <vuilachinh66@gmail.com>
2020-06-19 09:24:23 +0700

    [ACCOUNT] commit API for table account
    (cherry picked from commit 0d57c43ab2775a169f75e93532b8b9e8d83a6d49)

---
+++ src/main/java/com/example/fjob/service/controller/account/AccountController.java
@@ -0,0 +1,128 @@
+package com.example.fjob.service.controller.account;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.fjob.lib.dataset.account.AccountDataset;
+import com.example.fjob.lib.dataset.otp.OTPDataset;
+import com.example.fjob.service.service.account.AccountService;
+
+@RestController
+@RequestMapping("fjob/v1")
+public class AccountController {
+AccountService service;
+
+@Autowired
+public AccountController(AccountService service) {
+	this.service = service;
+}
+
+//insert new account
+
+@PostMapping("/create-account")
+public ResponseEntity<HttpStatus> insertNewAccount(
+		@RequestBody AccountDataset account) {
+	if(service.insertNewAccount(account) > 0) {
+		return new ResponseEntity<HttpStatus>(HttpStatus.CREATED);
+	}
+	return new ResponseEntity<HttpStatus>(HttpStatus.BAD_REQUEST);
+}
+
+//disable account
+@PutMapping("/disable-account")
+public ResponseEntity<HttpStatus> disableAccount(@RequestParam("username") String username){
+	service.disableAccount(username);
+			return new ResponseEntity<HttpStatus>(HttpStatus.OK);
+}
+
+//reset password
+@PutMapping("/reset-password")
+public ResponseEntity<Integer> resetPassword(@RequestBody AccountDataset account){
+	Integer result = service.resetPassword(account);
+	if(result > 0) 
+		return new ResponseEntity<>(result,HttpStatus.OK);
+	return new ResponseEntity<>(result,HttpStatus.BAD_REQUEST);
+}
+//get all user
+@GetMapping("/get-all-user")
+public ResponseEntity<List<AccountDataset>> getAllUser(){
+	return new ResponseEntity<>(service.getAllUser(),HttpStatus.OK);
+}
+//count no user
+@GetMapping("/count-user")
+public ResponseEntity<Integer> countNumberUser(){
+	return new ResponseEntity<>(service.countNumberUser(),HttpStatus.OK);
+}
+//verify account 
+@PutMapping("/verify-account")
+public ResponseEntity<Integer> verifyAccount(@RequestBody OTPDataset otp){
+	Integer result = service.verifyAccount(otp);
+	if(result > 0)
+	return new ResponseEntity<>(result,HttpStatus.OK);
+	return new ResponseEntity<Integer>(result,HttpStatus.NOT_ACCEPTABLE);
+}
+
+//update balance
+@PutMapping("/update-balance")
+public ResponseEntity<Integer> updateBalance(@RequestParam("balance") String balance, @RequestParam("userName") String userName){
+	Integer result = service.updateBalance(balance, userName);
+	if(result > 0)
+	return new ResponseEntity<>(result,HttpStatus.OK);
+	return new ResponseEntity<Integer>(result,HttpStatus.NOT_ACCEPTABLE);
+}
+
+//update profile
+@PutMapping("/update-profile")
+public ResponseEntity<Integer> updateProfile(@RequestBody AccountDataset account){
+	Integer result = service.updateProfile(account);
+	if(result > 0)
+	return new ResponseEntity<>(result,HttpStatus.OK);
+	return new ResponseEntity<Integer>(result,HttpStatus.NOT_ACCEPTABLE);
+}
+
+//verify student
+@PutMapping("/verify-student")
+public ResponseEntity<Integer> verifyStudent(@RequestParam("userName") String userName,@RequestParam("email") String email){
+	Integer result = service.verifyStudent(userName, email);
+	if(result > 0)
+	return new ResponseEntity<>(result,HttpStatus.OK);
+	return new ResponseEntity<Integer>(result,HttpStatus.NOT_ACCEPTABLE);
+}
+
+//get contact
+@GetMapping("/get-contact")
+public ResponseEntity<AccountDataset> getContact(@RequestParam("userName") String userName){
+	AccountDataset result = service.getContact(userName);
+	if(result != null )
+	return new ResponseEntity<>(result,HttpStatus.OK);
+	return new ResponseEntity<AccountDataset>(result,HttpStatus.BAD_REQUEST);
+}
+
+//get all infor
+@GetMapping("/get-all-infor")
+public ResponseEntity<AccountDataset> getAllInfor(@RequestParam("userName") String userName){
+	AccountDataset result = service.getAllInfor(userName);
+	if(result != null )
+	return new ResponseEntity<>(result,HttpStatus.OK);
+	return new ResponseEntity<AccountDataset>(result,HttpStatus.BAD_REQUEST);
+}
+
+//get infor for visiter
+@GetMapping("/get-infor-for-visiter")
+public ResponseEntity<AccountDataset> getInforForVisiter(@RequestParam("userName") String userName){
+	AccountDataset result = service.getInforForVisiter(userName);
+	if(result != null )
+	return new ResponseEntity<>(result,HttpStatus.OK);
+	return new ResponseEntity<AccountDataset>(result,HttpStatus.BAD_REQUEST);
+}
+}
---
+++ src/main/java/com/example/fjob/service/service/account/AccountService.java
@@ -0,0 +1,63 @@
+package com.example.fjob.service.service.account;
+
+import java.util.List;
+
+import com.example.fjob.lib.dataset.account.AccountDataset;
+import com.example.fjob.lib.dataset.otp.OTPDataset;
+
+public interface AccountService {
+	 
+	//insert new account
+	
+int insertNewAccount(AccountDataset account);
+
+
+//disable account
+
+int disableAccount(String username);
+
+//reset password
+
+int resetPassword(AccountDataset account);
+
+//get all user
+
+List<AccountDataset> getAllUser();
+
+//count number of user
+
+int countNumberUser();
+
+//verify account
+
+int verifyAccount(OTPDataset otp);
+
+//update balance
+
+int updateBalance(String balance,String userName);
+
+//update profile
+
+int updateProfile(AccountDataset account);
+
+////update password
+//
+//int updatePassword(String password);
+
+// verify student
+
+int verifyStudent(String userName,String email);
+
+//get contact
+
+AccountDataset getContact(String userName);
+
+//get all 
+
+AccountDataset getAllInfor(String userName);
+
+//get infor for visited 
+
+AccountDataset getInforForVisiter(String userName);
+
+}
