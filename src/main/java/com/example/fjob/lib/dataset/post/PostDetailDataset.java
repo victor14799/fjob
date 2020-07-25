9d6ed8d1d1819cd6b7c8526c483ffc8aa410a150
tyhuynh99 <tyhuynh1199@gmail.com>
2020-07-25 23:25:07 +0700

    hot fix post detail budget

--- src/main/java/com/example/fjob/lib/dataset/post/PostDetailDataset.java
+++ src/main/java/com/example/fjob/lib/dataset/post/PostDetailDataset.java
@@ -84,7 +84,7 @@ public class PostDetailDataset {
     }
 
     public String getBudget() {
-        return CommonUtils.formatMoney(this.budget);
+        return this.budget;
     }
 
     public void setBudget(String budget) {
